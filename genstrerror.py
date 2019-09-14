64a780ed327ac312a0535106944325d8dac2c7df
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2019-09-14 21:26:49 +0900

    Remove unused script

--- genstrerror.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-for i in range(0, 256):
-    print('''\
-  case NGHTTP3_ERR_HTTP_MALFORMED_FRAME - {n:#04x}:
-    return "ERR_HTTP_MALFORMED_FRAME_{n:#04X}";'''.format(n = i))
