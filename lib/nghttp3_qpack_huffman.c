8ac4fc9434f9a2d4a0d846be289c8ee64a2f4bbb
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2019-10-05 00:55:23 +0900

    Speed up huffman encoding

--- lib/nghttp3_qpack_huffman.c
+++ lib/nghttp3_qpack_huffman.c
@@ -29,73 +29,7 @@
 #include <assert.h>
 #include <stdio.h>
 
-/*
- * Encodes huffman code |sym| into |*dest_ptr|, whose least |rembits|
- * bits are not filled yet.  The |rembits| must be in range [1, 8],
- * inclusive.  At the end of the process, the |*dest_ptr| is updated
- * and points where next output should be placed. The number of
- * unfilled bits in the pointed location is returned.
- */
-static uint8_t *huffman_encode_sym(uint8_t *dest, size_t *prembits,
-                                   const nghttp3_qpack_huffman_sym *sym) {
-  size_t nbits = sym->nbits;
-  size_t rembits = *prembits;
-  uint32_t code = sym->code;
-
-  /* We assume that sym->nbits <= 32 */
-  if (rembits > nbits) {
-    *dest |= (uint8_t)(code << (rembits - nbits));
-    *prembits = rembits - nbits;
-    return dest;
-  }
-
-  if (rembits == nbits) {
-    *dest++ |= (uint8_t)code;
-    *prembits = 8;
-    return dest;
-  }
-
-  *dest++ |= (uint8_t)(code >> (nbits - rembits));
-
-  nbits -= rembits;
-  if (nbits & 0x7) {
-    /* align code to MSB byte boundary */
-    code <<= 8 - (nbits & 0x7);
-  }
-
-  /* fast path, since most code is less than 8 */
-  if (nbits < 8) {
-    *dest = (uint8_t)code;
-    *prembits = 8 - nbits;
-    return dest;
-  }
-
-  /* handle longer code path */
-  if (nbits > 24) {
-    *dest++ = (uint8_t)(code >> 24);
-    nbits -= 8;
-  }
-
-  if (nbits > 16) {
-    *dest++ = (uint8_t)(code >> 16);
-    nbits -= 8;
-  }
-
-  if (nbits > 8) {
-    *dest++ = (uint8_t)(code >> 8);
-    nbits -= 8;
-  }
-
-  if (nbits == 8) {
-    *dest++ = (uint8_t)code;
-    *prembits = 8;
-    return dest;
-  }
-
-  *dest = (uint8_t)code;
-  *prembits = 8 - nbits;
-  return dest;
-}
+#include "nghttp3_conv.h"
 
 size_t nghttp3_qpack_huffman_encode_count(const uint8_t *src, size_t len) {
   size_t i;
@@ -110,23 +44,34 @@ size_t nghttp3_qpack_huffman_encode_count(const uint8_t *src, size_t len) {
 
 uint8_t *nghttp3_qpack_huffman_encode(uint8_t *dest, const uint8_t *src,
                                       size_t srclen) {
-  size_t rembits = 8;
-  size_t i;
   const nghttp3_qpack_huffman_sym *sym;
+  const uint8_t *end = src + srclen;
+  uint64_t code = 0;
+  size_t nbits = 0;
+  uint32_t x;
 
-  for (i = 0; i < srclen; ++i) {
-    sym = &huffman_sym_table[src[i]];
-    if (rembits == 8) {
-      *dest = 0;
+  for (; src != end;) {
+    sym = &huffman_sym_table[*src++];
+    code |= (uint64_t)sym->code << (32 - nbits);
+    nbits += sym->nbits;
+    if (nbits < 32) {
+      continue;
     }
-    dest = huffman_encode_sym(dest, &rembits, sym);
+    x = htonl(code >> 32);
+    memcpy(dest, &x, 4);
+    dest += 4;
+    code <<= 32;
+    nbits -= 32;
   }
-  /* 256 is special terminal symbol, pad with its prefix */
-  if (rembits < 8) {
-    /* if rembits < 8, we should have at least 1 buffer space
-       available */
-    sym = &huffman_sym_table[256];
-    *dest++ |= (uint8_t)(sym->code >> (sym->nbits - rembits));
+
+  for (; nbits >= 8;) {
+    *dest++ = (uint8_t)(code >> 56);
+    code <<= 8;
+    nbits -= 8;
+  }
+
+  if (nbits) {
+    *dest++ = (uint8_t)((code >> 56) | ((1 << (8 - nbits)) - 1));
   }
 
   return dest;
