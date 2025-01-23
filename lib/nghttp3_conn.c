24d43447d57008c114788f1501f08ebb76fe70d9
mbuhl <git@moritzbuhl.de>
2025-01-23 08:48:47 +0100

    Avoid STOP_SENDING for unknown stream type in case fin is set.

--- lib/nghttp3_conn.c
+++ lib/nghttp3_conn.c
@@ -607,6 +607,9 @@ nghttp3_ssize nghttp3_conn_read_uni(nghttp3_conn *conn, nghttp3_stream *stream,
     break;
   case NGHTTP3_STREAM_TYPE_UNKNOWN:
     nconsumed = (nghttp3_ssize)srclen;
+    if (fin) {
+      break;
+    }
 
     rv = conn_call_stop_sending(conn, stream, NGHTTP3_H3_STREAM_CREATION_ERROR);
     if (rv != 0) {
